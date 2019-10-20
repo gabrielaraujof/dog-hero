98d4cfd66fb9efea9e87ade6ea25e10b5fc4c6fb
Gabriel Araujo <gabrielfeear@gmail.com>
2019-10-20 00:24:03 -0300

    Create avatar component

---
+++ src/app/shared/components/avatar/avatar.component.ts
@@ -0,0 +1,43 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  HostBinding,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+
+@Component({
+  selector: 'dh-avatar',
+  template: `
+    <img [src]="this.sourceFile" [alt]="this.alt" />
+    <span
+      *ngIf="!this.smallSize && this.superhero"
+      dh-icon
+      src="images/hero-badge.svg"
+      size="small"
+    ></span>
+  `,
+  styleUrls: ['./avatar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AvatarComponent {
+  @Input() src = '';
+
+  @Input() alt = '';
+
+  @Input() size: 'medium' | 'small' = 'medium';
+
+  @HostBinding('class.avatar--superhero')
+  @Input()
+  superhero = false;
+
+  @HostBinding('class.avatar--small')
+  get smallSize() {
+    return this.size === 'small';
+  }
+
+  get sourceFile() {
+    const sideSize = this.size === 'medium' ? 64 : 32;
+    return `${this.src}?w=${sideSize}&h=${sideSize}`;
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
 import { IconComponent } from './components/icon/icon.component';
 import { FavoriteComponent } from './components/favorite/favorite.component';
+import { AvatarComponent } from './components/avatar/avatar.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ButtonComponent, IconComponent, FavoriteComponent],
-  exports: [ButtonComponent, IconComponent, FavoriteComponent],
+  declarations: [
+    ButtonComponent,
+    IconComponent,
+    FavoriteComponent,
+    AvatarComponent,
+  ],
+  exports: [ButtonComponent, IconComponent, FavoriteComponent, AvatarComponent],
 })
 export class SharedModule {}
